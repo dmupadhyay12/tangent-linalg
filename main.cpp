8be02a7193720941b341532f32fbdcd8fd8d89f1
upadhyaydhruv <dmupadhyay12@gmail.com>
2025-08-17 21:43:46 -0700

    Initial matrix data structure, basic operator, and matmul implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 #include "include/matrix.hpp"
+#include <Eigen/Dense>
+#include <benchmark/benchmark.h>
 
-int main() {
-  tangent::Matrix<int, 1, 3> matrix_1 = {{2, 1, 3}};
-  tangent::Matrix<int, 3, 2> matrix_2 = {{1, 2, 3, 5, 4, 6}};
-  auto matrix_3 = matrix_1 * matrix_2;
+template <int M, int K, int N> static void BM_Tangent(benchmark::State &s) {
+  tangent::Matrix<double, M, K> A;
+  tangent::Matrix<double, K, N> B;
+  for (int i = 0; i < M; i++)
+    for (int j = 0; j < K; j++)
+      A(i, j) = (i + j) % 7 + 0.1;
+  for (int i = 0; i < K; i++)
+    for (int j = 0; j < N; j++)
+      B(i, j) = (i * 2 - j) % 5 + 0.2;
 
-  matrix_3.print();
+  const double flops = 2.0 * M * K * N;
+  for (auto _ : s) {
+    benchmark::DoNotOptimize(A);
+    benchmark::DoNotOptimize(B);
+    auto C = A * B;
+    benchmark::DoNotOptimize(C);
+    benchmark::ClobberMemory();
+  }
+  s.counters["GFLOP/s"] =
+      benchmark::Counter(flops, benchmark::Counter::kIsIterationInvariantRate,
+                         benchmark::Counter::kIs1000);
 }
+
+template <int M, int K, int N> static void BM_Eigen(benchmark::State &s) {
+  Eigen::setNbThreads(1);
+  Eigen::Matrix<double, M, K> A;
+  Eigen::Matrix<double, K, N> B;
+  Eigen::Matrix<double, M, N> C;
+  A.setRandom();
+  B.setRandom();
+  const double flops = 2.0 * M * K * N;
+  for (auto _ : s) {
+    benchmark::DoNotOptimize(A);
+    benchmark::DoNotOptimize(B);
+    C.noalias() = A * B;
+    benchmark::DoNotOptimize(C);
+    benchmark::ClobberMemory();
+  }
+  s.counters["GFLOP/s"] =
+      benchmark::Counter(flops, benchmark::Counter::kIsIterationInvariantRate,
+                         benchmark::Counter::kIs1000);
+}
+
+BENCHMARK(BM_Tangent<3, 3, 3>);
+BENCHMARK(BM_Eigen<3, 3, 3>);
+BENCHMARK(BM_Tangent<64, 64, 64>);
+BENCHMARK(BM_Eigen<64, 64, 64>);
+BENCHMARK(BM_Tangent<128, 128, 128>);
+BENCHMARK(BM_Eigen<128, 128, 128>);
+BENCHMARK_MAIN();
